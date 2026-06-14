6993b0e6e3cc687e8f2d2963940d39228477c3f2
Markinatorina <markinatorina@hotmail.com>
2026-06-14 11:56:14 +0200

    Clean up base data context surface

--- MSAVA-INF/Contexts/BaseDataContext.cs
+++ MSAVA-INF/Contexts/BaseDataContext.cs
@@ -1,61 +1,48 @@
-using MSAVA_INF.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MSAVA_INF.Models;
 
-namespace MSAVA_INF.Contexts
+namespace MSAVA_INF.Contexts;
+
+public class BaseDataContext : DbContext
 {
-    public class BaseDataContext : DbContext
+    public BaseDataContext(DbContextOptions<BaseDataContext> options) : base(options)
     {
-        public BaseDataContext(DbContextOptions<BaseDataContext> options) : base(options)
-        {
-            //todo
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Owner - AccessGroup one-to-many relationship
-            modelBuilder.Entity<AccessGroupDB>()
-                .HasOne(ag => ag.Owner)
-                .WithMany()
-                .HasForeignKey(ag => ag.OwnerId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            // User - InviteCode many-to-one relationship
-            modelBuilder.Entity<UserDB>()
-                .HasOne(u => u.InviteCode)
-                .WithMany()
-                .HasForeignKey(u => u.InviteCodeId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            // InviteCode - Owner one-to-many relationship
-            modelBuilder.Entity<InviteCodeDB>()
-                .HasOne(ic => ic.Owner)
-                .WithMany()
-                .HasForeignKey(ic => ic.OwnerId)
-                .OnDelete(DeleteBehavior.Restrict);
-        }
-
-        // User entities
-        public DbSet<InviteCodeDB> InviteCodes { get; set; }
-        public DbSet<UserDB> Users { get; set; }
-        public DbSet<JwtDB> Jwts { get; set; }
-
-        // File entities
-        public DbSet<SavedFileReferenceDB> FileRefs { get; set; }
-        public DbSet<SavedFileDataDB> FileData { get; set; }
-
-        // Access group entities
-        public DbSet<AccessCodeDB> AccessCodes { get; set; }
-        public DbSet<AccessGroupDB> AccessGroups { get; set; }
-
-        // Log entities
-        public DbSet<AccessLogDB> AccessLogs { get; set; }
-        public DbSet<UserLogDB> UserLogs { get; set; }
-        public DbSet<ErrorLogDB> ErrorLogs { get; set; }
-        public DbSet<GroupLogDB> GroupLogs { get; set; }
-        public DbSet<InviteLogDB> InviteLogs { get; set; }
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AccessGroupDB>()
+            .HasOne(ag => ag.Owner)
+            .WithMany()
+            .HasForeignKey(ag => ag.OwnerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<UserDB>()
+            .HasOne(u => u.InviteCode)
+            .WithMany()
+            .HasForeignKey(u => u.InviteCodeId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<InviteCodeDB>()
+            .HasOne(ic => ic.Owner)
+            .WithMany()
+            .HasForeignKey(ic => ic.OwnerId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+
+    public DbSet<InviteCodeDB> InviteCodes => Set<InviteCodeDB>();
+    public DbSet<UserDB> Users => Set<UserDB>();
+    public DbSet<JwtDB> Jwts => Set<JwtDB>();
+
+    public DbSet<SavedFileReferenceDB> FileRefs => Set<SavedFileReferenceDB>();
+    public DbSet<SavedFileDataDB> FileData => Set<SavedFileDataDB>();
+
+    public DbSet<AccessCodeDB> AccessCodes => Set<AccessCodeDB>();
+    public DbSet<AccessGroupDB> AccessGroups => Set<AccessGroupDB>();
+
+    public DbSet<AccessLogDB> AccessLogs => Set<AccessLogDB>();
+    public DbSet<UserLogDB> UserLogs => Set<UserLogDB>();
+    public DbSet<ErrorLogDB> ErrorLogs => Set<ErrorLogDB>();
+    public DbSet<GroupLogDB> GroupLogs => Set<GroupLogDB>();
+    public DbSet<InviteLogDB> InviteLogs => Set<InviteLogDB>();
 }
---
+++ MSAVA.Tests/BaseDataContextTests.cs
@@ -0,0 +1,97 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using MSAVA_INF.Contexts;
+using MSAVA_INF.Models;
+
+namespace MSAVA_App.Tests;
+
+public class BaseDataContextTests
+{
+    [Test]
+    public void PublicDbSetPropertiesExposeExpectedEntityTypes()
+    {
+        using var context = CreateContext();
+        var expectedEntityTypes = new Dictionary<string, Type>
+        {
+            [nameof(BaseDataContext.InviteCodes)] = typeof(InviteCodeDB),
+            [nameof(BaseDataContext.Users)] = typeof(UserDB),
+            [nameof(BaseDataContext.Jwts)] = typeof(JwtDB),
+            [nameof(BaseDataContext.FileRefs)] = typeof(SavedFileReferenceDB),
+            [nameof(BaseDataContext.FileData)] = typeof(SavedFileDataDB),
+            [nameof(BaseDataContext.AccessCodes)] = typeof(AccessCodeDB),
+            [nameof(BaseDataContext.AccessGroups)] = typeof(AccessGroupDB),
+            [nameof(BaseDataContext.AccessLogs)] = typeof(AccessLogDB),
+            [nameof(BaseDataContext.UserLogs)] = typeof(UserLogDB),
+            [nameof(BaseDataContext.ErrorLogs)] = typeof(ErrorLogDB),
+            [nameof(BaseDataContext.GroupLogs)] = typeof(GroupLogDB),
+            [nameof(BaseDataContext.InviteLogs)] = typeof(InviteLogDB)
+        };
+
+        var dbSetProperties = typeof(BaseDataContext)
+            .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+            .Where(property => property.PropertyType.IsGenericType)
+            .Where(property => property.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>))
+            .ToDictionary(
+                property => property.Name,
+                property => property.PropertyType.GenericTypeArguments[0]);
+
+        dbSetProperties.Should().BeEquivalentTo(expectedEntityTypes);
+        foreach (var (propertyName, entityType) in expectedEntityTypes)
+        {
+            typeof(BaseDataContext).GetProperty(propertyName)!.GetValue(context).Should().NotBeNull();
+            context.Model.FindEntityType(entityType).Should().NotBeNull();
+        }
+    }
+
+    [Test]
+    public void OnModelCreatingKeepsUserOwnedRelationshipsRestrictDelete()
+    {
+        using var context = CreateContext();
+
+        FindForeignKey(context.Model, typeof(AccessGroupDB), nameof(AccessGroupDB.OwnerId))
+            .DeleteBehavior
+            .Should()
+            .Be(DeleteBehavior.Restrict);
+        FindForeignKey(context.Model, typeof(UserDB), nameof(UserDB.InviteCodeId))
+            .DeleteBehavior
+            .Should()
+            .Be(DeleteBehavior.Restrict);
+        FindForeignKey(context.Model, typeof(InviteCodeDB), nameof(InviteCodeDB.OwnerId))
+            .DeleteBehavior
+            .Should()
+            .Be(DeleteBehavior.Restrict);
+    }
+
+    private static IForeignKey FindForeignKey(IModel model, Type entityType, string propertyName)
+    {
+        var entity = model.FindEntityType(entityType)
+            ?? throw new InvalidOperationException($"Entity {entityType.Name} was not found in the EF model.");
+
+        return entity
+            .GetForeignKeys()
+            .Single(foreignKey => foreignKey.Properties.Any(property => property.Name == propertyName));
+    }
+
+    private static BaseDataContext CreateContext()
+    {
+        var options = new DbContextOptionsBuilder<BaseDataContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        return new TestDataContext(options);
+    }
+
+    private sealed class TestDataContext : BaseDataContext
+    {
+        public TestDataContext(DbContextOptions<BaseDataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<SavedFileDataDB>().Ignore(fileData => fileData.Metadata);
+        }
+    }
+}
